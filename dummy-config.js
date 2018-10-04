57ae4b70191034870df38b646903ea9506e5595d
Moey Design <5884seiya@gmail.com>
2018-10-04 10:39:32 -0400

    dummy config file for reference

---
+++ dummy-config.js
@@ -0,0 +1,92 @@
+{
+  "stories": [
+    {
+      "storyId": 1,
+      "title": "Future Factory 4.0",
+      "category": "BUSINESS",
+      "summary": "Full automated manufacturing lines powered by 5G Network.",
+      "sections": [
+        {
+          "sectionType": "movie",
+          "sectionTitle": "Intro",
+          "talkingPoints": [
+            {
+              "heading": "Talking Point 1",
+              "text": "Lorem ipsum dolor sit amet, consecteur adipisicing elit."
+            },
+            {
+              "heading": "Talking Point 2",
+              "text": "Lorem ipsum dolor sit amet, consecteur adipisicing elit."
+            }
+          ]
+        },
+        {
+          "sectionType": "mission",
+          "sectionTitle": "Mission 1",
+          "missionId": 1,
+          "hasTwoPlayerMode": false,
+          "replayable": true,
+        },
+        {
+          "sectionType": "mission",
+          "sectionTitle": "Mission 2",
+          "missionId": 2,
+          "hasTwoPlayerMode": true,
+          "replayable": true,
+        },
+        {
+          "sectionType": "movie",
+          "sectionTitle": "Outro",
+          "talkingPoints": [
+            {
+              "heading": "Talking Point 1",
+              "text": "Lorem ipsum dolor sit amet, consecteur adipisicing elit."
+            },
+          ]
+        }
+      ],
+      "movieVersion": [
+        {
+          "label": "Intro",
+          "talkingPoints": [
+            {
+              "heading": "Talking Point 1",
+              "text": "Lorem ipsum dolor sit amet, consecteur adipisicing elit."
+            },
+            {
+              "heading": "Talking Point 2",
+              "text": "Lorem ipsum dolor sit amet, consecteur adipisicing elit."
+            }
+          ]
+        },
+        {
+          "label": "Mission 1",
+          "talkingPoints": [
+            {
+              "heading": "Talking Point 1",
+              "text": "Lorem ipsum dolor sit amet, consecteur adipisicing elit."
+            }
+          ]
+        },
+        {
+          "label": "Mission 2",
+          "talkingPoints": [
+            {
+              "heading": "Talking Point 1",
+              "text": "Lorem ipsum dolor sit amet, consecteur adipisicing elit."
+            }
+          ]
+        },
+        {
+          "label": "Outro",
+          "talkingPoints": [
+            {
+              "heading": "Talking Point 1",
+              "text": "Lorem ipsum dolor sit amet, consecteur adipisicing elit."
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
