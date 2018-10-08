95234cd0090668eb48a052403f4b529484c78717
Moey Design <5884seiya@gmail.com>
2018-10-08 14:11:50 -0400

    created demo directory

---
+++ demo/demo-frontend/src/store/store.js
@@ -0,0 +1,13 @@
+import state from './store-state';
+import mutations from './store-mutations';
+import actions from './store-actions';
+import getters from './store-getters';
+import plugins from './store-plugins';
+
+export default {
+  state,
+  mutations,
+  actions,
+  getters,
+  plugins,
+};
