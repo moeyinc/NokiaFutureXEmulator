e7aed6677fc03643ccadc1fbee58f7a8bdc08469
Moey Design <5884seiya@gmail.com>
2018-10-11 18:36:39 -0400

    built app

--- demo/demo-backend/public/static/message-config.js
+++ demo/demo-backend/public/static/message-config.js
@@ -1,111 +1,127 @@
-export default [
-  {
-    type: 'login',
-  },
-  {
-    type: 'logout',
-  },
-  {
-    type: 'start-story',
-    params: [
-      {
-        name: 'story',
-        options: [1, 2, 3],
-        value: 1,
-      },
-    ],
-  },
-  {
-    type: 'start-story-prerendered',
-    params: [
-      {
-        name: 'story',
-        options: [1, 2, 3],
-        value: 1,
-      },
-    ],
-  },
-  {
-    type: 'play',
-  },
-  {
-    type: 'pause',
-  },
-  {
-    type: 'end-story',
-  },
-  {
-    type: 'start-mission',
-    params: [
-      {
-        name: 'mission',
-        options: [1, 2],
-        value: 1,
-      },
-      {
-        name: 'autoPlay',
-        options: [true, false],
-        value: false,
-      },
-    ],
-    networkParams: [
-      {
-        name: 'latency',
-        options: [0, 1, 2, 3, 4, 5],
-        value: 5,
-      },
-      {
-        name: 'bandwidth',
-        options: [0, 1, 2, 3, 4, 5],
-        value: 5,
-      },
-      {
-        name: 'reliability',
-        options: [0, 1, 2, 3, 4, 5],
-        value: 5,
-      },
-      {
-        name: 'security',
-        options: [0, 1, 2, 3, 4, 5],
-        value: 5,
-      },
-    ],
-  },
-  {
-    type: 'end-mission',
-  },
-  {
-    type: 'calibrate',
-  },
-  {
-    type: 'end-calibration',
-  },
-  {
-    type: 'set-network',
-    networkParams: [
-      {
-        name: 'latency',
-        options: [0, 1, 2, 3, 4, 5],
-        value: 5,
-      },
-      {
-        name: 'bandwidth',
-        options: [0, 1, 2, 3, 4, 5],
-        value: 5,
-      },
-      {
-        name: 'reliability',
-        options: [0, 1, 2, 3, 4, 5],
-        value: 5,
-      },
-      {
-        name: 'security',
-        options: [0, 1, 2, 3, 4, 5],
-        value: 5,
-      },
-    ],
-  },
-  {
-    type: 'play-story-outro',
-  },
-];
+export default {
+  facilitatorToGame: [
+    {
+      type: 'login',
+    },
+    {
+      type: 'logout',
+    },
+    {
+      type: 'start-story',
+      params: [
+        {
+          name: 'story',
+          options: [1, 2, 3],
+          value: 1,
+        },
+      ],
+    },
+    {
+      type: 'start-story-prerendered',
+      params: [
+        {
+          name: 'story',
+          options: [1, 2, 3],
+          value: 1,
+        },
+      ],
+    },
+    {
+      type: 'play',
+    },
+    {
+      type: 'pause',
+    },
+    {
+      type: 'end-story',
+    },
+    {
+      type: 'start-mission',
+      params: [
+        {
+          name: 'mission',
+          options: [1, 2],
+          value: 1,
+        },
+        {
+          name: 'autoPlay',
+          options: [true, false],
+          value: false,
+        },
+      ],
+      networkParams: [
+        {
+          name: 'latency',
+          options: [0, 1, 2, 3, 4, 5],
+          value: 5,
+        },
+        {
+          name: 'bandwidth',
+          options: [0, 1, 2, 3, 4, 5],
+          value: 5,
+        },
+        {
+          name: 'reliability',
+          options: [0, 1, 2, 3, 4, 5],
+          value: 5,
+        },
+        {
+          name: 'security',
+          options: [0, 1, 2, 3, 4, 5],
+          value: 5,
+        },
+      ],
+    },
+    {
+      type: 'end-mission',
+    },
+    {
+      type: 'calibrate',
+    },
+    {
+      type: 'end-calibration',
+    },
+    {
+      type: 'set-network',
+      networkParams: [
+        {
+          name: 'latency',
+          options: [0, 1, 2, 3, 4, 5],
+          value: 5,
+        },
+        {
+          name: 'bandwidth',
+          options: [0, 1, 2, 3, 4, 5],
+          value: 5,
+        },
+        {
+          name: 'reliability',
+          options: [0, 1, 2, 3, 4, 5],
+          value: 5,
+        },
+        {
+          name: 'security',
+          options: [0, 1, 2, 3, 4, 5],
+          value: 5,
+        },
+      ],
+    },
+    {
+      type: 'play-story-outro',
+    },
+  ],
+  gameToFacilitator: [
+    {
+      type: 'completed-story-intro',
+    },
+    {
+      type: 'completed-story',
+    },
+    {
+      type: 'completed-mission',
+    },
+    {
+      type: 'completed-story-interlude',
+    },
+  ],
+};
