b8ad07f546a07789f243d6c3f788d423291d4b7d
Seiya Kobayashi <5884seiya@gmail.com>
2019-04-11 20:57:36 -0400

    fixing overlay order issue

--- components/overlays/OverlayWrapper.vue
+++ components/overlays/OverlayWrapper.vue
@@ -42,6 +42,7 @@ export default {
   width: 100%
   height: 100%
   background-color: rgba(1, 12, 66, .85)
+  -webkit-transform: translate3d(0,0,0)
   &.scrollable
     overflow: scroll
     -webkit-overflow-scrolling: touch
