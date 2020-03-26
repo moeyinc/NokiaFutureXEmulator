a91eb0c08ffde4ea873914e5cfa95b8b63e78384
Seiya Kobayashi <5884seiya@gmail.com>
2020-03-26 00:28:17 -0400

    create a new data.json file when it doesn't exist

--- facilitator-app/api/routes/experience.js
+++ facilitator-app/api/routes/experience.js
@@ -8,26 +8,77 @@ router.get('/experience/config', (req, res, next) => {
   res.json(config);
 });
 
-router.get('/experience/selected-sleeve-id', (req, res, next) => {
-  const data = fs.readFileSync(path.resolve(__dirname, '../data.json'));
-  const parsedData = JSON.parse(data);
-  res.json(parsedData);
+router.get('/experience/selected-sleeve-id', async (req, res, next) => {
+  let data;
+  try {
+    data = await readExperienceData();
+  } catch (err) {
+    res.status(500).send({error: err});
+    return;
+  }
+  res.json(data);
 });
 
-router.post('/experience/selected-sleeve-id', (req, res, next) => {
+router.post('/experience/selected-sleeve-id', async (req, res, next) => {
   const sleeveId = req.body.sleeveId;
-  const filepath = path.resolve(__dirname, '../data.json');
-  const data = fs.readFileSync(filepath);
-  const parsedData = JSON.parse(data)
-  parsedData.selectedSleeveId = sleeveId;
-  const newData = JSON.stringify(parsedData);
-  fs.writeFile(filepath, newData, 'utf8', (err) => {
-    if (err) res.status(500).send({error: 'Error on writing JSON'});
-    else {
-      console.log('New sleeveId: ', sleeveId);
-      res.send('OK');
-    }
-  });
+  let data;
+  try {
+    data = await readExperienceData();
+  } catch (err) {
+    res.status(500).send({error: err});
+    return;
+  }
+
+  data.selectedSleeveId = sleeveId;
+
+  try {
+    await writeExperienceData(data);
+  } catch (err) {
+    res.status(500).send({error: err});
+    return;
+  }
+
+  res.send('OK');
 });
+
+
+/**
+ * readExperienceData
+ *
+ */
+async function readExperienceData() {
+  const filepath = path.resolve(__dirname, '../data.json');
+  if (!fs.existsSync(filepath)) {
+    try {
+      await fs.promises.writeFile(filepath, '{}', 'utf8');
+    } catch (err) {
+      throw new Error('Error on writing JSON');
+    }
+    return {};
+  } else {
+    let data;
+    try {
+      data = await fs.promises.readFile(filepath, 'utf8');
+    } catch (err) {
+      throw new Error('Error on reading JSON');
+    }
+    return JSON.parse(data);
+  }
+}
+
+
+/**
+ * writeExperienceData
+ *
+ * @param {!Object} data
+ */
+async function writeExperienceData(data) {
+  const filepath = path.resolve(__dirname, '../data.json');
+  try {
+    await fs.promises.writeFile(filepath, JSON.stringify(data), 'utf8');
+  } catch (err) {
+    throw new Error('Error on writing JSON');
+  }
+}
 
 module.exports = router;
